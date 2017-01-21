f52abc5c724c7ae92fc7bd8250529b8e29ba3db2
SitingSong <1056284551@qq.com>
2017-01-21 11:01:02 +0800

    lecture 06

--- lecture 06/maze/maze.js
+++ lecture 06/maze/maze.js
@@ -1,53 +1,44 @@
-var maze = (function() {
-	var flag = 0;
-	var changeWall = "";
-	
-	function begin() {
-		flag = 1;
-		$("#" + changeWall).attr("class", "wall");
-		changeWall = "";
-		$("#msg").text(" ");
-	}
+var flag = 0;
+var changeWall = "";
 
-	function failed(wall) {
-		if (flag == 1) {
-			flag = 0;
-			changeWall = wall;
-			$("#" + wall).attr("class", "changed");
-			$("#msg").text("You Lose");
-		}
-	}
+window.onload = function main () {
+	document.getElementById("start").onmouseover = function() {begin();}
+	document.getElementById("end").onmouseover = function() {end();}
+	document.getElementById("up-middle").onmouseover = function() {failed("up-middle");}
+	document.getElementById("mid-left").onmouseover = function() {failed("mid-left");}
+	document.getElementById("mid-middle").onmouseover = function() {failed("mid-middle");}
+	document.getElementById("mid-right").onmouseover = function() {failed("mid-right");}
+	document.getElementById("down-left").onmouseover = function() {failed("down-left");}
+	document.getElementById("down-right").onmouseover = function() {failed("down-right");}
+	document.getElementById("maze").onmouseleave = function() {cheat()};
+}
 
-	function end() {
-		if (flag == 1)
-			$("#msg").text("You Win");
-		else
-			$("#msg").text("Don't cheat, you should start start from the \'S\' and move to the \'E\' inside the maze!");
-	}
+function begin() {
+	flag = 1;
+	changeWall = "";
+	document.getElementById("msg").textContent = "";
+}
 
-	function cheat() {
-		if (flag == 1)
-			flag = 0;
-		if (flag == 0 || changeWall != "")
-			$("#" + changeWall).attr("class", "wall");
+function failed(wall) {
+	if (flag == 1) {
+		flag = 0;
+		changeWall = wall;
+		document.getElementById(wall).className = "changed";
+		document.getElementById("msg").textContent = "You Lose";
 	}
+}
 
-	return {
-		begin: begin,
-		end: end,
-		failed: failed,
-		cheat: cheat
+function end() {
+	if (flag == 1) {
+		document.getElementById("msg").textContent = "You Win";
+	} else {
+		document.getElementById("msg").textContent = "Don't cheat, you should start start from the \'S\' and move to the \'E\' inside the maze!";
 	}
-})();
+}
 
-$(function() {
-	$("#start").mouseover(function() {maze.begin();});
-	$("#end").mouseover(function() {maze.end();});
-	$("#up-middle").mouseover(function() {maze.failed("up-middle");});
-	$("#mid-left").mouseover(function() {maze.failed("mid-left");});
-	$("#mid-middle").mouseover(function() {maze.failed("mid-middle");});
-	$("#mid-right").mouseover(function() {maze.failed("mid-right");});
-	$("#down-left").mouseover(function() {maze.failed("down-left");});
-	$("#down-right").mouseover(function() {maze.failed("down-right");});
-	$("#maze").mouseleave(function() {maze.cheat();});
-});
+function cheat() {
+	if (flag == 1)
+		flag = 0;
+	if (flag == 0 || changeWall != "")
+		document.getElementById(changeWall).className = "wall";
+}
--- lecture 06/mole/mole.js
+++ lecture 06/mole/mole.js
@@ -3,15 +3,21 @@ var timeId = null;        //指定setTimeout()的变量
 var playing = false;      //游戏是否开始
 var score = 0;            //分数
 var ran = 0;              //亮起的mole号码
-var point = $("#mole").children();
+var point = document.getElementsByName("aMole");
+
+window.onload = function main () {
+	document.getElementById("start-mole").onclick = function() {satrtGame();}
+	for (var i = 0; i < point.length; i++) {
+		point[i].onclick = function() {changeMole(this.id);}
+	};
+}
 
 function satrtGame() {
 	if (!playing) {
 		gameTime = 30;
 		score = 0;
 		playing = true;
-		$("#score").attr("value", score);
-		$("#status").attr("value", "Playing");
+		document.getElementById("status").value = "Playing";
 		timeShow();
 		moleShow();
 	} else {
@@ -20,7 +26,7 @@ function satrtGame() {
 }
 
 function timeShow() { //显示当前倒计时所剩时间
-    $("#time-count").attr("value", gameTime);
+    document.getElementById("time-count").value = gameTime;
     if(gameTime == 0) {
         GameOver();
         return;
@@ -34,10 +40,10 @@ function GameOver() {
     clearTimeout(timeId); //clearTime()方法返回setTimeout()的id
     playing = false;
     gameTime = 0;
-    $("#time-count").attr("value", gameTime);
+    document.getElementById("time-count").value = gameTime;
     point[ran].checked = false;
-    $("#status").attr("value", "Game Over");
-    alert("Game Over.\nYour score is: "+ score);
+    document.getElementById("status").value = "Game Over";
+    alert("Game Over.\nYour score is: "+score);
 }
 
 function moleShow() {
@@ -45,27 +51,23 @@ function moleShow() {
 	point[ran].checked = true;
 }
 
-function changeMole(that) {
+function changeMole(id) {
 	if (!playing) {
 		alert("请点击开始游戏");
-		that.checked = false;
+		document.getElementById(id).checked = false;
 	} else {
 		if (point[ran].checked == true) {
 			score++;
-			$("#score").attr("value",score);
-			that.checked = false;
+			document.getElementById("score").value = score;
+			document.getElementById(id).checked = false;
 			ran = parseInt(60 * Math.random());
 			point[ran].checked = true;
 		} else {
 			score--;
-			$("#score").attr("value", score);
-			that.checked = false;
+			document.getElementById("score").value = score;
+			document.getElementById(id).checked = false;
 			point[ran].checked = true;
 		}
 	}
-};
-
-$(function() {
-	$("#start-mole").click(function() {satrtGame();});
-	$("#mole").children().click(function() {changeMole(this);});
-});
+		
+}
